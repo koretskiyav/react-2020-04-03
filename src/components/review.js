2f29ed8aaca88565dbcd525220e31e4965a149fc
VladimirTertyshnikov <59862303+VladimirTertyshnikov@users.noreply.github.com>
2020-04-08 21:02:56 +0300

    Update component Review - include Rate

--- src/components/review.js
+++ src/components/review.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Rate } from 'antd';
 
 export default function Review(props) {
   return (
     <div>
-      <p>{props.review.user}</p>
+      <p>
+        {props.review.user}
+        <Rate disabled defaultValue={props.review.rating} />
+      </p>
       <p>{props.review.text}</p>
     </div>
   );
