4003b40f399f4817ff65ead76a5e14632715459c
IgVSIII <40248988+IgVSIII@users.noreply.github.com>
2020-04-08 11:03:05 +0300

    test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/app';
 
 import { restaurants } from './fixtures';
-
+/*test*/
 ReactDOM.render(
   <App restaurants={restaurants} />,
   document.getElementById('root')
