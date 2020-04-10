29c0878ee4804cb0ebab58924550173058993308
Дроздова Елена Владимировна <e.drozdova@cft.ru>
2020-04-10 20:58:05 +0700

    Добавлена обертка  useMemo в resturant

--- src/components/restuarant.js
+++ src/components/restuarant.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Reviews from './reviews';
 import Menu from './menu';
 import { Rate } from 'antd';
 
 export default function Restuarant(props) {
-  const getAverageRating = reviews => {
-    return (
-      reviews.reduce((sum, current) => sum + current.rating, 0) / reviews.length
-    );
-  };
+  const avarageRate = useMemo(
+    () =>
+      props.restaurant.reviews.reduce(
+        (sum, current) => sum + current.rating,
+        0
+      ) / props.restaurant.reviews.length,
+    [props.restaurant]
+  );
 
   return (
     <div>
-      <Rate disabled value={getAverageRating(props.restaurant.reviews)} />
+      <Rate disabled value={avarageRate} />
       <div>
         <Menu restaurant={props.restaurant} />
         <Reviews reviews={props.restaurant.reviews} />
