61bff6b82f0a1dcc388f5ccf3854ce087c976de3
Irina Soldatenko <irina.soldatenko156@mail.ru>
2020-04-14 18:25:04 +0300

    fix: add test for user, text in review

--- src/components/reviews/review/review.test.js
+++ src/components/reviews/review/review.test.js
@@ -11,9 +11,29 @@ describe('Review', () => {
     const component = mount(<Review {...review} />);
     expect(
       component
-        .find('Rate')
+        .find('[data-id="review-rate"]')
         .at(0)
         .props().value
     ).toBe(review.rating);
+  });
+
+  it('should Typography.Title text equal user', () => {
+    const component = mount(<Review {...review} />);
+    expect(
+      component
+        .find('[data-id="review-title"]')
+        .at(0)
+        .text()
+    ).toBe(review.user);
+  });
+
+  it('should Typography.Text text equal text', () => {
+    const component = mount(<Review {...review} />);
+    expect(
+      component
+        .find('[data-id="review-text"]')
+        .at(0)
+        .text()
+    ).toBe(review.text);
   });
 });
