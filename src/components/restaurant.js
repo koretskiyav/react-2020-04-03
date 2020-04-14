8b7ccae1151c6219dfc82130a44dda974a87ed66
pakhomova <pakhomova@kraud.ru>
2020-04-14 11:59:11 +0300

    add useMemo

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Menu from './menu';
 import Reviews from './reviews';
 import { Rate } from 'antd';
 
 export default function Restaurant(props) {
-  const arrReviewsLength = props.restaurant.reviews.length;
+  const { reviews } = props.restaurant;
 
-  const ratingSum = props.restaurant.reviews.reduce(
-    (prev, val) => prev + val.rating,
-    0
+  const averageRating = useMemo(
+    () =>
+      Math.round(
+        (reviews.reduce((prev, val) => prev + val.rating, 0) / reviews.length) *
+          2
+      ) / 2,
+    [reviews]
   );
-
-  const averageRating = Math.round((ratingSum / arrReviewsLength) * 2) / 2;
 
   return (
     <div>
