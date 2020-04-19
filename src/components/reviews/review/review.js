4ccdcb0dc7b0293f2c3c7520a9df06f379602525
eydlin <a.aidlin@gmail.com>
2020-04-19 22:10:13 +0300

    modify Review to use key=>value, fix user display

--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import { Row, Col, Typography, Rate, Card } from 'antd';
 import styles from './review.module.css';
 
-const Review = ({ user, text, rating }) => (
+const Review = ({ review, user }) => (
   <Card className={styles.review} data-id="review-card">
     <Row type="flex" align="middle">
       <Col xs={24} md={18} align="left">
@@ -13,27 +14,38 @@ const Review = ({ user, text, rating }) => (
           level={4}
           data-id="review-user"
         >
-          {user}
+          {user.name}
         </Typography.Title>
         <Typography.Text className={styles.comment} data-id="review-text">
-          {text}
+          {review.text}
         </Typography.Text>
       </Col>
       <Col xs={8} md={6} align="right" className={styles.rateColumn}>
-        <Rate disabled value={rating} />
+        <Rate disabled value={review.rating} />
       </Col>
     </Row>
   </Card>
 );
 
 Review.propTypes = {
-  user: PropTypes.string,
-  text: PropTypes.string,
-  rating: PropTypes.number.isRequired
+  review: PropTypes.shape({
+    userId: PropTypes.string.isRequired,
+    text: PropTypes.string,
+    rating: PropTypes.number.isRequired
+  }).isRequired,
+  user: PropTypes.shape({
+    id: PropTypes.string,
+    name: PropTypes.string.isRequired
+  })
 };
 
 Review.defaultProps = {
-  user: 'Anonymous'
+  user: { name: 'Anonymous' }
 };
 
-export default Review;
+Review.mapStateToProps = (state, ownProps) => ({
+  review: state.reviews[ownProps.id],
+  user: state.users[state.reviews[ownProps.id].userId]
+});
+
+export default connect(Review.mapStateToProps)(Review);
