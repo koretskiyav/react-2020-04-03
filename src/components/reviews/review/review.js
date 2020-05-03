84f6eb48ad55585b14984ed0f2d31cc61e0f363c
Andrii Koretskyi <andriiko@wix.com>
2020-05-03 17:04:53 +0300

    add HT7.4

--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Row, Col, Typography, Rate, Card } from 'antd';
+import { CSSTransition } from 'react-transition-group';
 
 import { reviewWitUserSelector } from '../../../redux/selectors';
 
 import styles from './review.module.css';
+import animationStyles from './review-animation.module.css';
 
 const Review = ({ review }) => {
   if (!review) return null;
@@ -13,25 +15,27 @@ const Review = ({ review }) => {
   const { user = 'Anonymous', text, rating } = review;
 
   return (
-    <Card className={styles.review} data-id="review-card">
-      <Row type="flex" align="middle">
-        <Col xs={24} md={18} align="left">
-          <Typography.Title
-            className={styles.name}
-            level={4}
-            data-id="review-user"
-          >
-            {user}
-          </Typography.Title>
-          <Typography.Text className={styles.comment} data-id="review-text">
-            {text}
-          </Typography.Text>
-        </Col>
-        <Col xs={8} md={6} align="right" className={styles.rateColumn}>
-          <Rate disabled value={rating} />
-        </Col>
-      </Row>
-    </Card>
+    <CSSTransition timeout={500} classNames={animationStyles} in appear>
+      <Card className={styles.review} data-id="review-card">
+        <Row type="flex" align="middle">
+          <Col xs={24} md={18} align="left">
+            <Typography.Title
+              className={styles.name}
+              level={4}
+              data-id="review-user"
+            >
+              {user}
+            </Typography.Title>
+            <Typography.Text className={styles.comment} data-id="review-text">
+              {text}
+            </Typography.Text>
+          </Col>
+          <Col xs={8} md={6} align="right" className={styles.rateColumn}>
+            <Rate disabled value={rating} />
+          </Col>
+        </Row>
+      </Card>
+    </CSSTransition>
   );
 };
 
